06e11b7dd7064565d04740637f888ec5affa8fa7
Rory O’Kane <rory@roryokane.com>
2013-03-31 15:21:22 -0400

    add RSpec check to examples_spec

--- spec/acceptance/examples_spec.rb
+++ spec/acceptance/examples_spec.rb
@@ -32,6 +32,16 @@ describe "Regression on" do
             "Please create in examples/output!"
         end
       end
+      
+      it "has any and all RSpec tests pass", :ruby => 1.9 do
+        # this will also pass if the example has no RSpec tests
+        stdout_and_stderr_str, status = Open3.capture2e("rspec #{example}")
+        tests_passed = status.success?
+        unless tests_passed
+          fail "RSpec tests for example failed:\n" + stdout_and_stderr_str
+        end
+      end
+      
     end
   end
 end
