00799eb3810cbe87a30998e4527846d86f652f9f
Rory O’Kane <rory@roryokane.com>
2013-04-07 22:08:51 -0400

    change new examples’ indentation to project standard

--- example/consistent_newlines.rb
+++ example/consistent_newlines.rb
@@ -14,59 +14,59 @@ require 'parslet/rig/rspec'
 
 
 class ConsistentNewlineTextParser < Parslet::Parser
-	rule(:first_newline) do
-		str("\r").maybe >> str("\n")
-	end
-	
-	rule(:newline) do
-		dynamic do |source, context|
-			begin
-				str(context.captures[:newline])
-			rescue Parslet::Scope::NotFound
-				first_newline.capture(:newline)
-			end
-		end
-	end
-	
-	
-	rule(:word) { match('\w').repeat }
-	
-	rule(:line_content) { word }
-	
-	rule(:lines) do
-		(line_content.as(:line_content) >> newline).repeat >>
-		line_content.as(:line_content)
-	end
-	
-	root(:lines)
+  rule(:first_newline) do
+    str("\r").maybe >> str("\n")
+  end
+  
+  rule(:newline) do
+    dynamic do |source, context|
+      begin
+        str(context.captures[:newline])
+      rescue Parslet::Scope::NotFound
+        first_newline.capture(:newline)
+      end
+    end
+  end
+  
+  
+  rule(:word) { match('\w').repeat }
+  
+  rule(:line_content) { word }
+  
+  rule(:lines) do
+    (line_content.as(:line_content) >> newline).repeat >>
+    line_content.as(:line_content)
+  end
+  
+  root(:lines)
 end
 
 
 describe ConsistentNewlineTextParser do
-	let(:parser) { described_class.new }
-	
-	let(:n1) { "\r\n" }
-	let(:n2) { "\n" }
-	
-	it "allows text with the same newline throughout" do
-		parser.should parse("")
-		parser.should parse("one")
-		
-		# for each type of newline individually
-		[n1, n2].each do |n|
-			# the parser should parse these strings containing that newline
-			parser.should parse("one#{n}")
-			parser.should parse("one#{n}#{n}#{n}")
-			parser.should parse("one#{n}two#{n}three")
-			parser.should parse("one#{n}two#{n}three#{n}")
-		end
-	end
-	
-	it "rejects text with mixed newlines" do
-		parser.should_not parse("one#{n1}two#{n2}")
-		parser.should_not parse("one#{n2}two#{n1}")
-		parser.should_not parse("one#{n1}two#{n2}three")
-		parser.should_not parse("one#{n1}two#{n1}three#{n2}")
-		parser.should_not parse("one#{n2}two#{n1}three#{n2}")
-	end
+  let(:parser) { described_class.new }
+  
+  let(:n1) { "\r\n" }
+  let(:n2) { "\n" }
+  
+  it "allows text with the same newline throughout" do
+    parser.should parse("")
+    parser.should parse("one")
+    
+    # for each type of newline individually
+    [n1, n2].each do |n|
+      # the parser should parse these strings containing that newline
+      parser.should parse("one#{n}")
+      parser.should parse("one#{n}#{n}#{n}")
+      parser.should parse("one#{n}two#{n}three")
+      parser.should parse("one#{n}two#{n}three#{n}")
+    end
+  end
+  
+  it "rejects text with mixed newlines" do
+    parser.should_not parse("one#{n1}two#{n2}")
+    parser.should_not parse("one#{n2}two#{n1}")
+    parser.should_not parse("one#{n1}two#{n2}three")
+    parser.should_not parse("one#{n1}two#{n1}three#{n2}")
+    parser.should_not parse("one#{n2}two#{n1}three#{n2}")
+  end
 end
--- example/count_captured_chars.rb
+++ example/count_captured_chars.rb
@@ -11,38 +11,38 @@ require 'parslet/rig/rspec'
 
 
 class NAThenNBParser < Parslet::Parser
-	root(:a_s_then_b_s)
-	rule(:a_s_then_b_s) do
-		str('a').repeat(0).capture(:a_s).as(:a_s) >>
-		dynamic do |source, context|
-			num_a_s = context.captures[:a_s].size
-			str('b').repeat(num_a_s, num_a_s).as(:b_s)
-		end
-	end
+  root(:a_s_then_b_s)
+  rule(:a_s_then_b_s) do
+    str('a').repeat(0).capture(:a_s).as(:a_s) >>
+    dynamic do |source, context|
+      num_a_s = context.captures[:a_s].size
+      str('b').repeat(num_a_s, num_a_s).as(:b_s)
+    end
+  end
 end
 
 
 describe NAThenNBParser do
-	let(:parser) { described_class.new }
-	
-	it "parses ‘a’s and ‘b’s of the same number" do
-		parser.should parse("")
-		parser.should parse("ab")
-		parser.should parse("aabb")
-		parser.should parse("aaabbb")
-	end
-	
-	it "rejects uneven ‘a’s and ‘b’s" do
-		parser.should_not parse("a")
-		parser.should_not parse("b")
-		parser.should_not parse("aab")
-		parser.should_not parse("abb")
-		parser.should_not parse("aaab")
-		parser.should_not parse("abbb")
-	end
-	
-	it "rejects other invalid input" do
-		parser.should_not parse("baa")
-		parser.should_not parse("bbaa")
-	end
+  let(:parser) { described_class.new }
+  
+  it "parses ‘a’s and ‘b’s of the same number" do
+    parser.should parse("")
+    parser.should parse("ab")
+    parser.should parse("aabb")
+    parser.should parse("aaabbb")
+  end
+  
+  it "rejects uneven ‘a’s and ‘b’s" do
+    parser.should_not parse("a")
+    parser.should_not parse("b")
+    parser.should_not parse("aab")
+    parser.should_not parse("abb")
+    parser.should_not parse("aaab")
+    parser.should_not parse("abbb")
+  end
+  
+  it "rejects other invalid input" do
+    parser.should_not parse("baa")
+    parser.should_not parse("bbaa")
+  end
 end
